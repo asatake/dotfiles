ceb55e0f24b88269658a9505623906327ff76c31
asatake <asataken@gmail.com>
2022-10-17 22:26:14 +0900

    :+1: fix asdf conditions

--- macos/init.sh
+++ macos/init.sh
@@ -57,36 +57,26 @@ curl -fsSL https://raw.githubusercontent.com/asatake/dotfiles/main/shared/alacri
 ## asdf
 
 if [[ -z $(asdf list direnv) ]]; then
-    echo "direnv has already installed."
-else
     asdf plugin add direnv https://github.com/asdf-community/asdf-direnv
     asdf install direnv 2.32.0
     asdf global direnv 2.32.0
 fi
 if [[ -z $(asdf list golang) ]]; then
-    echo "golang has already installed."
-else
     asdf plugin add golang https://github.com/kennyp/asdf-golang.git
     asdf install golang 1.19.2
     asdf global golang 1.19.2
 fi
 if [[ -z $(asdf list nodejs) ]]; then
-    echo "nodejs has already installed."
-else
     asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
     asdf install nodejs lts
     asdf global nodejs lts
 fi
 if [[ -z $(asdf list python) ]]; then
-    echo "python has already installed."
-else
     asdf plugin add python https://github.com/asdf-community/asdf-python.git
     asdf install python 3.10.8
     asdf global python 3.10.8
 fi
 if [[ -z $(asdf list ruby) ]]; then
-    echo "ruby has already installed."
-else
     asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
     asdf install ruby 3.0.1
 fi
