f6ffef91e4ab969be110e083f26c0ff05c0416ad
asatake <asataken@gmail.com>
2025-07-18 15:57:27 +0900

    :shower: remove direnv

--- macos/init.sh
+++ macos/init.sh
@@ -97,10 +97,6 @@ fi
 
 
 ## asdf
-if [[ -z $(asdf list direnv) ]]; then
-  asdf plugin add direnv https://github.com/asdf-community/asdf-direnv
-  asdf direnv setup --shell zsh --version latest
-fi
 if [[ -z $(asdf list golang) ]]; then
   asdf plugin add golang https://github.com/kennyp/asdf-golang.git
   asdf install golang 1.24.3
